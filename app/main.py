9cb38c675b78aace09ef593d8141c2ca95ab910c
luisospino <luisospinoa@gmail.com>
2021-11-06 15:30:33 +0000

    avance ok

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="api de botones", description="esta es una api de control", version="1.0.0")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/fan/{status}")
+async def index(status: str):
+    print('el estado es' + status)
+    # aquí va el código de para rabbit
+    return status
--- app/proxy.py
+++ app/proxy.py
@@ -7,53 +7,29 @@ import paho.mqtt.client as mqtt #import the client1
 
 broker_address="40.71.125.21"
 
-# my_bucket = "device1"
-# my_org    = "taller3"
-# my_token  = "-77eJIW6Bd3NWSLnHsnsfT5U90P-SKgeYx8R2JI5lxRPHVFKR_O5Wl4kkZ7WpvNGO-OukFqAVWbP7tDW9tMYYg=="
+my_bucket = "device1"
+my_org    = "taller3"
+my_token  = "YJSP3DWMheK7UQcXngRpFAYIqvhLtaVn25BYnursFzix9A3L5V4qNtFDuZXKrk422eBSCQrAnInKDIo78GKRzA=="
+client_influx = InfluxDBClient(url="http://influxdb:8086", token=my_token, org=my_org)
 
-# client = InfluxDBClient(url="http://influxdb:8086", token=my_token, org=my_org)
+def process_function(mesage):
+    #mesage = mesage.decode("utf-8")
+    print(mesage)      
+    write_api = client_influx.write_api(write_options=SYNCHRONOUS)
+    query_api = client_influx.query_api()
+    point = Point("Medicion").tag("Ubicacion", "Santa Marta").field("temperatura", float(mesage)).time(datetime.utcnow(), WritePrecision.NS)
+    write_api.write(my_bucket, my_org, point)    
+    return
 
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+    msg = str(message.payload.decode("utf-8"))
+    process_function(msg)
+    #enviar la data al influx
 
 client = mqtt.Client("test") #create new instance
 client.on_message=on_message
 client.connect(broker_address) #connect to broker
 client.loop_start()
 client.subscribe("device1", qos=1)
-client.publish("device1","Yo soy un topico melo")#publish
-# time.sleep(10) # wait
-client.loop_forever() #stop the loop
-
-print("..s.s")
-
-# def process_function(mesage):
-#   mesage = mesage.decode("utf-8")
-#   print(mesage)      
-#   write_api = client.write_api(write_options=SYNCHRONOUS)
-#   query_api = client.query_api()
-#   point = Point("Medicion").tag("Ubicacion", "Santa Marta").field("temperatura", float(mesage)).time(datetime.utcnow(), WritePrecision.NS)
-#   write_api.write(my_bucket, my_org, point)    
-#   return
-
-# while 1:
-#   url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@rabbit:5672/%2f')
-#   params = pika.URLParameters(url)
-#   connection = pika.BlockingConnection(params)
-#   channel = connection.channel() # start a channel
-#   channel.queue_declare(queue='mensajes') # Declare a queue
-#   # create a function which is called on incoming messages
-#   def callback(ch, method, properties, body):
-#     process_function(body)
-
-#   # set up subscription on the queue
-#   channel.basic_consume('mensajes',
-#     callback,
-#     auto_ack=True)
-
-#   # start consuming (blocks)
-#   channel.start_consuming()
-#   connection.close()
+# client.publish("device1", 12)#publish
+client.loop_forever() #loop forever
