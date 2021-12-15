e7ca254cfc37f2bc6c25482ba7ec6a605630ce49
luisospino <luisospinoa@gmail.com>
2021-12-15 04:24:13 +0000

    Ajuste de motobomba

--- app/proxy.py
+++ app/proxy.py
@@ -11,30 +11,32 @@ my_bucket_potencia = os.environ.get("DOCKER_INFLUXDB_INIT_BUCKET_POTENCIA")
 my_bucket_water = os.environ.get("DOCKER_INFLUXDB_INIT_BUCKET_WATER")
 my_bucket_motobomba = os.environ.get("DOCKER_INFLUXDB_INIT_BUCKET_MOTOBOMBA")
 
-print(my_bucket_voltaje, my_bucket_corriente, my_bucket_potencia, my_bucket_water)
+buckets = [my_bucket_voltaje, my_bucket_corriente, my_bucket_potencia, my_bucket_water, my_bucket_motobomba]
+print("Iniciando...")
+print("Buckets ->", buckets, end = "\n\n")
 
 my_org = os.environ.get("DOCKER_INFLUXDB_INIT_ORG")
 my_token  = os.environ.get("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN")
 client_influx = InfluxDBClient(url="http://influxdb:8086", token=my_token, org=my_org)
 
-def proccess_function(mesage, bucket):
-    print(mesage)
+def proccess_function(message, bucket):
+    print(message)
     
     try:
-        mesage = float(mesage)
+        message = float(message)
         if(bucket == 'water'):
-            mesage = ((180 - mesage)/180) * 100
+            message = ((50 - message)/50) * 100
             
         write_api = client_influx.write_api(write_options=SYNCHRONOUS)
         query_api = client_influx.query_api()
-        point = Point(bucket+'_target').field(bucket, mesage).time(datetime.utcnow(), WritePrecision.NS)
+        point = Point(bucket+'_target').field(bucket, message).time(datetime.utcnow(), WritePrecision.NS)
         write_api.write(bucket, my_org, point)
         return
     except ValueError:
         return
 
 def on_message(client, userdata, message):
-    print("nuevo mensaje -> ", message.topic)
+    print("nuevo mensaje ->", message.topic)
     msg = str(message.payload.decode("utf-8"))
     
     if message.topic == 'voltaje':
